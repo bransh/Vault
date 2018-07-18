92431431adbe134834199c5505924d86ec00d7a0
bransh <bransh@gmail.com>
2018-07-18 11:16:52 -0300

    Update SDPT.py

--- SDPT.py
+++ SDPT.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 #
 # SDPT (Simple DNS Poisoner Tool) sniffes for DNS queries going through your attacker host
-# and provides fake answers to the specified DNS type A queries. It also forward other queries
+# and provides fake answers to the specified DNS type A queries. It also forwards other queries
 # different than type A and those pointing to FQDN that are not specifically included within the
 # fake list.
 #
